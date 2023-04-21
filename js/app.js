73ca17a6230e5064fbc5b5c6dd0965a75a6819fa
LaurenceWang <61631525+LaurenceWang@users.noreply.github.com>
2023-04-21 10:43:22 +0200

    added loi de poisson

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("hi");
+import * as math from './math.js';
+
+/*const lambda = 2;
+const k = 5;
+const p = math.poisson(lambda, k);
+console.log(`La probabilité d'observer ${k} événements avec un paramètre lambda de ${lambda} est ${p}`);*/
+
+
+function nbMaladeMacro(Y, lambda) {
+	let nbMalades = 0;
+	for (let i = 0; i < Y; i++) {
+	  const p = math.poisson(lambda, i);
+	  //const rand = Math.random();
+	  if (math.rand() < p) {
+		nbMalades = i;
+		break;
+	  }
+	}
+	return nbMalades;
+}
+
+const Y = 1000;
+const lambda = 20;
+const nbMalades = nbMaladeMacro(Y, lambda);
+console.log(`Le nombre de malades contaminés dans une population de ${Y} personnes avec un paramètre lambda de ${lambda} est ${nbMalades}`);
---
+++ js/math.js
@@ -0,0 +1,15 @@
+export function rand(){
+	return Math.random();
+}
+
+export function poisson(lambda, k) {
+	return (Math.pow(lambda, k) * Math.exp(-lambda)) / factorial(k);
+  }
+  
+export function factorial(n) {
+	if (n === 0 || n === 1) {
+	  return 1;
+	} else {
+	  return n * factorial(n - 1);
+	}
+}
