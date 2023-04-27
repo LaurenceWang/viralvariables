18e9d6b4adc9838af8c3aa5af1e38f32d41afafe
LaurenceWang <61631525+LaurenceWang@users.noreply.github.com>
2023-04-27 18:03:16 +0200

    cleaned code and added event dice

--- js/app.js
+++ js/app.js
@@ -5,47 +5,65 @@ const k = 5;
 const p = math.poisson(lambda, k);
 console.log(`La probabilité d'observer ${k} événements avec un paramètre lambda de ${lambda} est ${p}`);*/
 
+const eventType = ["depistage", "aleas", "guerison"];
+
 window.onload = function() {
-	var modal = document.getElementById('modal');
-	var modalClose = document.getElementById('modal-close');
-  
+	let modal = document.getElementById('modal');
+	let modalClose = document.getElementById('modal-close');
+
 	modal.style.display = 'block';
   
+	let gameContainer = document.getElementById('game');
+
 	modalClose.onclick = function() {
 	  modal.style.display = 'none';
+	  gameContainer.style.display='block';
 	  gameLoop();
 	};
   };
   
   function gameLoop() { 
-    var message = document.createElement('p');
-    message.innerHTML = 'Un clic sur le bouton = un jour';
-    var container = document.querySelector('.container');
-    container.appendChild(message);
 
-    var count = 0;
-	var malades = 0;
-    var nbMalades = 0;
-    var button = document.createElement('button');
-    button.innerHTML = 'Fin de tour';
-    button.onclick = function() {
-		malades += nbMalades;
-		var message = document.createElement('p');
-		message.innerHTML = `Le nombre de personnes contaminées JOUR ${count} est ${malades}`;
-		message.id = 'malades-message';
-		count ++;
-		// on clean le message précédent
-		var prevMessage = document.getElementById('malades-message');
-		if (prevMessage) {
-			container.replaceChild(message, prevMessage);
-		} else {
-			container.appendChild(message);
-		}
+	//let container = document.getElementById('popEvolution');
+	let nextRoundbtn = document.getElementById('nextRound');
+	let eventBtn = document.getElementById('dice');
+	console.log(eventBtn)
+
+    /*let count = 0;
+	let malades = 0;
+    let nbMalades = 0;*/
+
+    nextRoundbtn.onclick = ()=> {
+		maladeEvolution();	
+	} 
+
+	eventBtn.onclick = ()=>{
+		let message = document.createElement('p');
+		let resultEvent = eventType[math.rollEventDice(eventType.length)];
+		message.innerHTML = resultEvent;
+		let container = document.getElementById('event');
+		container.innerHTML = '';
+		container.appendChild(message);
+
+	}
+
 	
-		nbMalades = nbMaladeMacro(Y, lambda);
-	};
 	
-    container.appendChild(button);
+}
+
+
+
+function maladeEvolution(){
+	let container = document.getElementById('popEvolution');
+	let popText = `Le nombre de personnes contaminées JOUR ${count} est ${malades} <br> Population en bonne santé : ${originalPopulation - malades}`;
+	let message = document.createElement('p');
+	message.innerHTML = popText;
+	container.innerHTML = '';
+	container.appendChild(message);
+	count ++;
+	population -= nbMalades;
+	nbMalades = nbMaladeMacro(population, lambda);
+	malades += nbMalades;
 }
 
   
@@ -53,7 +71,7 @@ window.onload = function() {
 function nbMaladeMacro(Y, lambda) { // elle renvoit beaucoup de fois 0, c'est abérant ou j'ai pas compris un truc ?
 	let nbMalades = 0;
 	for (let i = 0; i < Y; i++) {
-	  const p = math.poisson(lambda, i);
+	  const p = math.poisson(lambda,i);
 	  //const rand = Math.random();
 	  if (math.rand() < p) {
 		nbMalades = i;
@@ -63,8 +81,11 @@ function nbMaladeMacro(Y, lambda) { // elle renvoit beaucoup de fois 0, c'est ab
 	return nbMalades;
 }
 
-
-const Y = 1000;
-const lambda = 20;
-const nbMalades = nbMaladeMacro(Y, lambda);
-console.log(`Le nombre de malades contaminés dans une population de ${Y} personnes avec un paramètre lambda de ${lambda} est ${nbMalades}`);
+const originalPopulation = 100000;
+let population = originalPopulation;
+let lambda = 50;
+let count = 0;
+let malades = 0;
+let nbMalades = 0;
+//const nbMalades = nbMaladeMacro(Y, lambda);
+//console.log(`Le nombre de malades contaminés dans une population de ${Y} personnes avec un paramètre lambda de ${lambda} est ${nbMalades}`);
--- js/math.js
+++ js/math.js
@@ -4,7 +4,7 @@ export function rand(){
 
 export function poisson(lambda, k) {
 	return (Math.pow(lambda, k) * Math.exp(-lambda)) / factorial(k);
-  }
+}
   
 export function factorial(n) {
 	if (n === 0 || n === 1) {
@@ -12,4 +12,8 @@ export function factorial(n) {
 	} else {
 	  return n * factorial(n - 1);
 	}
+}
+
+export function rollEventDice(nbEventType) {
+	return Math.floor(Math.random() * nbEventType);
 }
