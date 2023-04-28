3b19bf27f69310fc63cad1ec2556a56635d1c2c2
LaurenceWang <61631525+LaurenceWang@users.noreply.github.com>
2023-04-28 22:28:00 +0200

    updated game loop and added surprise event

--- js/app.js
+++ js/app.js
@@ -1,91 +1,179 @@
 import * as math from './math.js';
 
-/*const lambda = 2;
-const k = 5;
-const p = math.poisson(lambda, k);
-console.log(`La probabilité d'observer ${k} événements avec un paramètre lambda de ${lambda} est ${p}`);*/
-
-const eventType = ["depistage", "aleas", "guerison"];
-
-window.onload = function() {
-	let modal = document.getElementById('modal');
-	let modalClose = document.getElementById('modal-close');
-
-	modal.style.display = 'block';
-  
-	let gameContainer = document.getElementById('game');
-
-	modalClose.onclick = function() {
-	  modal.style.display = 'none';
-	  gameContainer.style.display='block';
-	  gameLoop();
-	};
-  };
-  
-  function gameLoop() { 
-
-	//let container = document.getElementById('popEvolution');
-	let nextRoundbtn = document.getElementById('nextRound');
-	let eventBtn = document.getElementById('dice');
-	console.log(eventBtn)
-
-    /*let count = 0;
-	let malades = 0;
-    let nbMalades = 0;*/
-
-    nextRoundbtn.onclick = ()=> {
-		maladeEvolution();	
-	} 
-
-	eventBtn.onclick = ()=>{
-		let message = document.createElement('p');
-		let resultEvent = eventType[math.rollEventDice(eventType.length)];
-		message.innerHTML = resultEvent;
-		let container = document.getElementById('event');
-		container.innerHTML = '';
-		container.appendChild(message);
-
-	}
-
-	
-	
-}
-
-
-
-function maladeEvolution(){
-	let container = document.getElementById('popEvolution');
-	let popText = `Le nombre de personnes contaminées JOUR ${count} est ${malades} <br> Population en bonne santé : ${originalPopulation - malades}`;
-	let message = document.createElement('p');
-	message.innerHTML = popText;
-	container.innerHTML = '';
-	container.appendChild(message);
-	count ++;
-	population -= nbMalades;
-	nbMalades = nbMaladeMacro(population, lambda);
-	malades += nbMalades;
-}
-
-  
-
-function nbMaladeMacro(Y, lambda) { // elle renvoit beaucoup de fois 0, c'est abérant ou j'ai pas compris un truc ?
-	let nbMalades = 0;
-	for (let i = 0; i < Y; i++) {
-	  const p = math.poisson(lambda,i);
-	  //const rand = Math.random();
-	  if (math.rand() < p) {
-		nbMalades = i;
-		break;
-	  }
-	}
-	return nbMalades;
-}
-
-const originalPopulation = 100000;
+//variables 
+const originalPopulation = 10000;
 let population = originalPopulation;
 let lambda = 50;
 let count = 0;
 let malades = 0;
 let nbMalades = 0;
+let addSick = 0;
+const eventType = ["depistage", "aleas", "guerison", "surprise"];
+
+let endOfRound = true;
+let eventEnded = true;
+
+//recuperation div
+let gameContainer =  document.getElementById('game');
+let popEvoContainer = document.getElementById('popEvolution');
+let eventContainer = document.getElementById('event');
+let eventNameContainer = document.getElementById('eventName');
+let eventContentContainer = document.getElementById('eventContent');
+
+//recuperation boutons
+let nextRoundbtn = document.getElementById('nextRound');
+let eventBtn = document.getElementById('dice');
+
+
+window.onload = function () {
+	let modal = document.getElementById('modal');
+	let modalClose = document.getElementById('modal-close');
+
+	modal.style.display = 'block';
+
+	modalClose.onclick = function () {
+		modal.style.display = 'none';
+		gameContainer.style.display = 'block';
+		
+	};
+};
+
+
+//fin de tour
+nextRoundbtn.onclick = () => {
+	maladeEvolution(addSick);
+	endOfRound = false;
+	eventEnded = false;
+	eventContainer.style.display = "none";
+	updateState()
+
+};
+
+eventBtn.onclick = () => {
+	let resultEvent = eventType[math.rollDice(eventType.length)];
+	addText(resultEvent, eventNameContainer);
+
+	//on appelle la fonction qui correspond à l'event
+	eval(resultEvent + "()");
+	eventContainer.style.display = "flex";
+};
+
+
+
+function updateState(){
+	//rendre les boutons inactifs
+	if (!endOfRound) {
+		nextRoundbtn.classList.add('disabled');
+	}else{
+		nextRoundbtn.classList.remove('disabled');
+	}
+
+	if (!eventEnded) {
+		eventBtn.classList.remove('disabled');
+	}else{
+		eventBtn.classList.add('disabled');
+	}
+};
+
+
+/***** fonctions des events *****/
+function depistage() {
+	addText("oups la fonction de dépistage n'est pas encore codée", eventContentContainer);
+	
+	endOfRound = true;
+	eventEnded = true;
+	updateState();
+}
+
+function aleas() {
+	addText("oups la fonction d'aleas n'est pas encore codée", eventContentContainer);
+	endOfRound = true;
+	eventEnded = true;
+	updateState();
+}
+
+function guerison() {
+	addText("oups la fonction de guérison n'est pas encore codée", eventContentContainer);
+	endOfRound = true;
+	eventEnded = true;
+	updateState();
+}
+
+function surprise() {
+
+	let playerNumber = math.rollDice(100); //on tire un nombre aléatoire entre 1 et 1000
+	let successNumber = math.successGeo();
+	let msgNumber = math.rollDice(4);
+
+	if (playerNumber > successNumber) {
+		addSick = math.rollDice(malades/2); //Valeur a ajuster ici 
+		addText(negSurprise(msgNumber, addSick), eventContentContainer)
+	
+		console.log("event negatif")
+	} else {
+		addSick = -math.rollDice(malades/2);
+		addText(posSurprise(msgNumber, addSick), eventContentContainer)
+	
+		console.log("event positif")
+	}
+
+	endOfRound = true;
+	eventEnded = true;
+	updateState();
+}
+
+
+/*** text ***/
+
+function posSurprise(messageNb, sickNb){
+	let posSurprises = [`Un médecin étranger en visite a guéri ${Math.abs(sickNb)} personnes ! Malheureusement il est rentré dans son pays après sa bonne action`, `Une variante étrange du virus a permi de l'éliminer chez ${Math.abs(sickNb)} personnes ! Malheureusement elle ne s'est pas répandue.`, `${Math.abs(sickNb)} ont mangé un doliprane...tiens ça va mieux !`, `${Math.abs(sickNb)} personnes ont suivi l'incroyable cours de probabilités d'IMAC2 et se sentent beaucoup mieux`]
+
+	return posSurprises[messageNb];
+}
+
+function negSurprise(messageNb, sickNb){
+	let negSurprises = [`${sickNb} personnes ont mangé un drôle de tacos hier...Oh`, `Le pays ennemi envoie une armée de moustiques porteur du virus !! ${sickNb} personnes ont été piqués`, `L'âge de départ à la retraite a encore reculé... le corps médical décide de faire grêve...${sickNb} personnes en plus attrapent le virus`, `${sickNb} étudiants ont enchainé des nuits blanches pour finir leurs projets...ils sont si fatigués que leur corp n'a pas produit d'anti-corps` ]
+
+	return negSurprises[messageNb];
+}
+
+function addText(text, div){
+	let message = document.createElement('p');
+	message.innerHTML = text;
+	div.innerHTML = '';
+	div.appendChild(message);
+}
+
+
+/** malade evolution **/
+
+function maladeEvolution(eventResult) {
+	count++;
+	population -= nbMalades;
+	nbMalades = math.poissonDistribution(lambda) + eventResult;
+	malades += nbMalades;
+	addSick = 0;
+
+	let popText = `${nbMalades} nouvelles personnes ont été contaminées aujourd'hui dont ${eventResult} causés par l'évènement de la veille. <br> Le nombre de personnes contaminées total JOUR ${count} est ${malades} <br> Population en bonne santé : ${originalPopulation - malades}`;
+	addText(popText, popEvoContainer);
+}
+
+//j'ai changé de fonction pour la loi de poisson cf math.js
+//
+//probablement à utiliser pour la vision particules
+function nbMaladeMacro(Y, lambda) { // elle renvoit beaucoup de fois 0, c'est abérant ou j'ai pas compris un truc ?
+	let nbMalades = 0;
+	for (let i = 0; i < Y; i++) {
+		const p = math.poisson(lambda, i);
+		//const rand = Math.random();
+		if (math.rand() < p) {
+			nbMalades = i;
+			break;
+		}
+	}
+	return nbMalades;
+}
+
+
 //const nbMalades = nbMaladeMacro(Y, lambda);
 //console.log(`Le nombre de malades contaminés dans une population de ${Y} personnes avec un paramètre lambda de ${lambda} est ${nbMalades}`);
--- js/math.js
+++ js/math.js
@@ -1,19 +1,63 @@
-export function rand(){
+export function rand() {
 	return Math.random();
 }
 
-export function poisson(lambda, k) {
-	return (Math.pow(lambda, k) * Math.exp(-lambda)) / factorial(k);
-}
-  
-export function factorial(n) {
-	if (n === 0 || n === 1) {
-	  return 1;
-	} else {
-	  return n * factorial(n - 1);
-	}
+export function poissonDistribution(lambda) {
+
+	let L = Math.exp(-lambda);
+	let p = 1.0;
+	let k = 0;
+
+	do {
+		k++;
+		p *= rand();
+	} while (p > L);
+
+	return k - 1;
 }
 
-export function rollEventDice(nbEventType) {
+export function poisson(lambda, k) {
+	const eLambda = Math.exp(-lambda); // e^-lambda
+	let proba = 1;
+	for (let i = 1; i <= k; i++) {
+		proba *= lambda / i; // lambda^k / k!
+	}
+	return eLambda * proba; // e^-lambda * (lambda^k / k!)
+}
+
+//version avec factorielle explose
+
+/*export function poisson(lambda, k) {
+	return (Math.pow(lambda, k) * Math.exp(-lambda)) / factorial(k);
+}*/
+
+/*export function factorial(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	} else {
+		return n * factorial(n - 1);
+	}
+}*/
+
+export function geometrique(p, k) {
+	return p * Math.pow((1 - p), k - 1);
+}
+
+export function successGeo() {
+	let n = Math.random(); // on tire un n random
+	let p = Math.random() / 10; //probabilité d'un succès -> plus la proba est petite et plus le nombres de tirages augmente
+
+	let k = 1;
+	let proba = geometrique(p, k);
+
+	while (proba < n) {
+		k++;
+		proba += geometrique(p, k);
+	}
+
+	return k; //nombre de tirages nécessaires pour avoir un succès
+}
+
+export function rollDice(nbEventType) {
 	return Math.floor(Math.random() * nbEventType);
 }
